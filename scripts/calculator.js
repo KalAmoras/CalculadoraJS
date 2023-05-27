dfe6543324c8f8a4d8efa9e1c83f10f34734836b
KalAmoras <kalamorasarquivo@gmail.com>
2023-05-26 23:29:19 -0300

    scripts

---
+++ scripts/calculator.js
@@ -0,0 +1,6 @@
+//Criado uma vez dentro de window, não é preciso mais usar
+//Document está dentro de window
+//Se quiser definir window como global é preciso declará-la
+window.calculator = new calcController();
+
+
